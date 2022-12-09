5206e8fc0664c652a195479113ba5da83388a21c
shanedunne <shanejjdunne@hotmail.com>
2022-12-09 19:52:49 +0100

    incorporate Chainlink VRF for randomness

--- contracts/GameContract.sol
+++ contracts/GameContract.sol
@@ -1,14 +1,20 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.1;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/utils/Base64.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+import "@chainlink/contracts/src/v0.8/VRFV2WrapperConsumerBase.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract CharacterCollector is ERC721URIStorage {
-
+contract CharacterCollector is
+    ERC721URIStorage,
+    VRFV2WrapperConsumerBase,
+    ConfirmedOwner
+{
     using Strings for uint256;
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
@@ -16,67 +22,191 @@ contract CharacterCollector is ERC721URIStorage {
     mapping(uint256 => uint256) public tokenIdToLevels;
     mapping(uint256 => string) public tokenIdToName;
 
-    constructor() ERC721 ("Character Collector", "CC"){
-}
-    function generateCharacter(uint256 tokenId) public returns(string memory){
-
-    bytes memory svg = abi.encodePacked(
-        '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
-        '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
-        '<rect width="100%" height="100%" fill="black" />',
-        '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',getName(tokenId),'</text>',
-        '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Levels: ",getLevels(tokenId),'</text>',
-        '</svg>'
+    // EVENTS
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(
+        uint256 requestId,
+        uint256[] randomWords,
+        uint256 payment
     );
-    return string(
-        abi.encodePacked(
-            "data:image/svg+xml;base64,",
-            Base64.encode(svg)
-        )    
-    );
-}
 
-function getLevels(uint256 tokenId) public view returns (string memory) {
-    uint256 levels = tokenIdToLevels[tokenId];
-    return levels.toString();
-}
+    uint256 fee = 0.0001 * 10**18;
+    bytes32 keyHash =
+        0x4b09e658ed251bcafeebbc69400383d49f344ace09b9576fe248bb02c003fe9f;
+    address linkAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    address wrapperAddress = 0x99aFAf084eBA697E584501b8Ed2c0B37Dd136693;
 
-function getName(uint256 tokenId) public view returns (string memory) {
-    string memory name = tokenIdToName[tokenId];
-    return name;
-}
+    uint32 callbackGasLimit = 100000;
 
-function getTokenURI(uint256 tokenId) public returns (string memory){
-    bytes memory dataURI = abi.encodePacked(
-        '{',
-            '"name": "Character Collector #', tokenId.toString(), '",',
+    // The default is 3, but you can set this higher.
+    uint16 requestConfirmations = 3;
+
+    // For this example, retrieve 2 random values in one request.
+    // Cannot exceed VRFV2Wrapper.getConfig().maxNumWords.
+    uint32 numWords = 1;
+
+    struct RequestStatus {
+        uint256 paid; // amount paid in link
+        bool fulfilled; // whether the request has been successfully fulfilled
+        uint256[] randomWords;
+    }
+    mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    constructor()
+        ERC721("Character Collector", "CC")
+        ConfirmedOwner(msg.sender)
+        VRFV2WrapperConsumerBase(linkAddress, wrapperAddress)
+    {}
+
+    function generateCharacter(uint256 tokenId) public returns (string memory) {
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
+            '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            getName(tokenId),
+            "</text>",
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
+            "Levels: ",
+            getLevels(tokenId),
+            "</text>",
+            "</svg>"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:image/svg+xml;base64,",
+                    Base64.encode(svg)
+                )
+            );
+    }
+
+    function getLevels(uint256 tokenId) public view returns (string memory) {
+        uint256 levels = tokenIdToLevels[tokenId];
+        return levels.toString();
+    }
+
+    function getName(uint256 tokenId) public view returns (string memory) {
+        string memory name = tokenIdToName[tokenId];
+        return name;
+    }
+
+    function getTokenURI(uint256 tokenId) public returns (string memory) {
+        bytes memory dataURI = abi.encodePacked(
+            "{",
+            '"name": "Character Collector #',
+            tokenId.toString(),
+            '",',
             '"description": "Collect and level characters",',
-            '"image": "', generateCharacter(tokenId), '"',
-        '}'
-    );
-    return string(
-        abi.encodePacked(
-            "data:application/json;base64,",
-            Base64.encode(dataURI)
+            '"image": "',
+            generateCharacter(tokenId),
+            '"',
+            "}"
+        );
+        return
+            string(
+                abi.encodePacked(
+                    "data:application/json;base64,",
+                    Base64.encode(dataURI)
+                )
+            );
+    }
+
+    function mint(string memory name) public {
+        _tokenIds.increment();
+        uint256 newItemId = _tokenIds.current();
+        _safeMint(msg.sender, newItemId);
+        tokenIdToLevels[newItemId] = 0;
+        tokenIdToName[newItemId] = name;
+        _setTokenURI(newItemId, getTokenURI(newItemId));
+    }
+
+    /*
+    function getRandomLevelUp() public returns (bytes32 requestId) {
+        require(
+            IERC20(LINK).balanceOf(address(this)) > fee,
+            "Not enough LINK - fill contract with faucet"
+        );
+        bytes32 levelUp = (requestRandomness(keyHash, fee) % 10) + 1;
+        return levelUp;
+    }
+    */
+
+    function requestRandomWords()
+        external
+        onlyOwner
+        returns (uint256 requestId)
+    {
+        requestId = requestRandomness(
+            callbackGasLimit,
+            requestConfirmations,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({
+            paid: VRF_V2_WRAPPER.calculateRequestPrice(callbackGasLimit),
+            randomWords: new uint256[](0),
+            fulfilled: false
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].paid > 0, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        emit RequestFulfilled(
+            _requestId,
+            _randomWords,
+            s_requests[_requestId].paid
+        );
+    }
+
+    function getRequestStatus(uint256 _requestId)
+        external
+        view
+        returns (
+            uint256 paid,
+            bool fulfilled,
+            uint256[] memory randomWords
         )
-    );
-}
+    {
+        require(s_requests[_requestId].paid > 0, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.paid, request.fulfilled, request.randomWords);
+    }
 
-function mint(string memory name) public {
-    _tokenIds.increment();
-    uint256 newItemId = _tokenIds.current();
-    _safeMint(msg.sender, newItemId);
-    tokenIdToLevels[newItemId] = 0;
-    tokenIdToName[newItemId] = name;
-    _setTokenURI(newItemId, getTokenURI(newItemId));
-}
+    /**
+     * Allow withdraw of Link tokens from the contract
+     */
+    function withdrawLink() public onlyOwner {
+        LinkTokenInterface link = LinkTokenInterface(linkAddress);
+        require(
+            link.transfer(msg.sender, link.balanceOf(address(this))),
+            "Unable to transfer"
+        );
+    }
+    /*
 
-function train(uint256 tokenId) public {
-    require(_exists(tokenId), "Please use an existing token");
-    require(ownerOf(tokenId) == msg.sender, "You must own this token to train it");
-    uint256 currentLevel = tokenIdToLevels[tokenId];
-    tokenIdToLevels[tokenId] = currentLevel + 1;
-    _setTokenURI(tokenId, getTokenURI(tokenId));
-}
-
+    function train(uint256 tokenId) public {
+        require(_exists(tokenId), "Please use an existing token");
+        require(
+            ownerOf(tokenId) == msg.sender,
+            "You must own this token to train it"
+        );
+        uint256 currentLevel = tokenIdToLevels[tokenId];
+        uint256 newLevelUp = getRandomLevelUp();
+        tokenIdToLevels[tokenId] = currentLevel + newLevelUp;
+        _setTokenURI(tokenId, getTokenURI(tokenId));
+    }
+    */
 }
