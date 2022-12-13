7682ddce814f4e5a6a4298c279324ac9257c6aca
shanedunne <shanejjdunne@hotmail.com>
2022-12-13 18:27:15 +0100

    fix an error where name is not being generated

--- contracts/GameContract.sol
+++ contracts/GameContract.sol
@@ -84,7 +84,7 @@ contract CharacterCollector is
             "<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>",
             '<rect width="100%" height="100%" fill="black" />',
             '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',
-            getName(),
+            getName(tokenId),
             "</text>",
             '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">',
             "Levels: ",
@@ -114,8 +114,7 @@ contract CharacterCollector is
     }
 
     // returns name of a token
-    function getName() public view returns (string memory) {
-        uint256 tokenId = getTokenId();
+    function getName(uint256 tokenId) public view returns (string memory) {
         string memory name = tokenIdToName[tokenId];
         return name;
     }
